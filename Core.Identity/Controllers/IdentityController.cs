266415d76f25e4279d6fa1fb4fc82b81f0293b9d
Dan <me@danielholcomb.com>
2022-11-20 14:50:43 -0700

    Add token logic

--- Core.Identity/Controllers/IdentityController.cs
+++ Core.Identity/Controllers/IdentityController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Core.Identity.Controllers
 {
@@ -7,17 +11,30 @@ namespace Core.Identity.Controllers
     public class IdentityController : ControllerBase
     {
         private readonly ILogger<IdentityController> _logger;
+        private readonly IConfiguration _config;
 
-        public IdentityController(ILogger<IdentityController> logger)
+        public IdentityController(ILogger<IdentityController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         [HttpGet]
         [Route("Token")]
-        public IActionResult Get()
+        public Task<IActionResult> Token()
         {
-            return Ok();
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config.GetSection("Jwt:Key").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", "design") }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = _config.GetSection("Jwt:Issuer").Value,
+                Audience = _config.GetSection("Jwt:Audience").Value,
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(tokenHandler.WriteToken(token));
         }
     }
 }
